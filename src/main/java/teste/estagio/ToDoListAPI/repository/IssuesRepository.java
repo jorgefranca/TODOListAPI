4071e9bb28d635a2d7f82821734ea662f87dac18
JorgeF <jorgeartur.f@gmail.com>
2021-12-07 14:04:16 -0300

    método findIssuesUsers

--- src/main/java/teste/estagio/ToDoListAPI/repository/IssuesRepository.java
+++ src/main/java/teste/estagio/ToDoListAPI/repository/IssuesRepository.java
@@ -3,12 +3,17 @@ package teste.estagio.ToDoListAPI.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import teste.estagio.ToDoListAPI.Issues;
 
-public interface IssuesRepository extends JpaRepository<Issues, Integer>{
+public interface IssuesRepository extends JpaRepository<Issues, Integer> {
 
 	List<Issues> findAll();
 	
+	@Query("SELECT A, B.nome FROM Issues A JOIN Users B ON(B.id=:id AND A.userId=:id)")
+	//@Query("SELECT A FROM Issues A WHERE id = (SELECT B.issues_Id FROM issues_users B WHERE B.users_Id=:id)")
+	List<Issues> findIssuesUsers(Integer id);
+	//@Query("SELECT Issues, Users FROM Issues, Users INNER JOIN Users WHERE Users.id= :id")
 	
 }
