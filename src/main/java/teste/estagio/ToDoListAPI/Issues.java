e0e06ba35b27130138209f5f9ecf5202cf88c8ca
JorgeF <jorgeartur.f@gmail.com>
2021-12-07 13:59:13 -0300

    anotação @manytomany

--- src/main/java/teste/estagio/ToDoListAPI/Issues.java
+++ src/main/java/teste/estagio/ToDoListAPI/Issues.java
@@ -4,10 +4,19 @@
  * */
 package teste.estagio.ToDoListAPI;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
 
 @Entity //entidade do banco de dados
+@Table(name = "Issues")
 public class Issues {
 
 	@Id //parâmetros do banco de dados
@@ -17,20 +26,35 @@ public class Issues {
 	private boolean todo;
 	private boolean doing;
 	private boolean done;
+	private Integer userId;
+
 	
+
+	@ManyToMany
+	@JoinTable(name="issues_users",
+	joinColumns = @JoinColumn(name = "issues_Id", referencedColumnName = "id"),
+	inverseJoinColumns = @JoinColumn(name = "users_Id", referencedColumnName = "id"))
+	//Users users;
+	private List<Users> users = new ArrayList<>(); //lista de usuários que podem ter aqui
+
+
 	
+
 	//constructores
 	public Issues() {
 		
 	}
 	
-	public Issues(Integer id, String descr, String createAT, boolean todo, boolean doing, boolean done) {
+	public Issues(Integer id, String descr, String createAT, boolean todo, boolean doing, boolean done, Users user) {
 		this.id = id;
 		this.descr = descr;
 		this.createAT = createAT;
 		this.todo = todo;
 		this.doing = doing;
 		this.done = done;
+		this.users.add(user);
+		this.userId=user.getId();
+		//this.users = users;//vai adicionar o usuário ao issues
 	}
 	
 	//setters e getters
@@ -69,5 +93,21 @@ public class Issues {
 	}
 	public void setDone(boolean done) {
 		this.done = done;
+	}
+
+	public List<Users> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<Users> users) {
+		this.users = users;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
 	}
 }
